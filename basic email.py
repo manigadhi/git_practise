8df3b7be1ff98ff25f7939437132b803cf5c9a63
manigadhi <mr.gadhimani430@gmail.com>
2017-06-18 11:57:41 +0530

    commiting the email file to git repo

---
+++ basic email.py
@@ -0,0 +1,8 @@
+import smtplib
+ 
+server = smtplib.SMTP('smtp.gmail.com', 587)
+server.starttls()
+server.login("gadhimani430@gmail.com", "9535895336")
+msg = "i love u veena"
+server.sendmail("gadhimani430@gmail.com", "manivna@gmail.com", "i love u veena")
+server.quit()
